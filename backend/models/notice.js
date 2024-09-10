21a045de4dc9e863247edb1a2be62f8370ce8702
mihirhebalkar <mihirhebalkar07@gmail.com>
2024-09-10 13:12:32 +0530

    Backend script added in package.json

--- backend/models/notice.js
+++ backend/models/notice.js
@@ -11,7 +11,7 @@ const noticeSchema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // Reference to the User model
+        ref: "User", 
         required: true,
     },
     date: {
